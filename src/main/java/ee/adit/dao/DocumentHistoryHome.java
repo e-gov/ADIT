b3a354827e4b21f0ee03d6ea7c44f9f70a0ff7e2
jaaklember <jaaklember@ria.ee>
2010-05-21 13:50:56 +0000

    Korralikud DAO klassid, korras Foreign-Key-dega.

---
+++ src/main/java/ee/adit/dao/DocumentHistoryHome.java
@@ -0,0 +1,123 @@
+package ee.adit.dao;
+
+// Generated 21.05.2010 16:49:44 by Hibernate Tools 3.2.4.GA
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class DocumentHistory.
+ * @see ee.adit.dao.DocumentHistory
+ * @author Hibernate Tools
+ */
+public class DocumentHistoryHome {
+
+	private static final Log log = LogFactory.getLog(DocumentHistoryHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext()
+					.lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException(
+					"Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(DocumentHistory transientInstance) {
+		log.debug("persisting DocumentHistory instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(DocumentHistory instance) {
+		log.debug("attaching dirty DocumentHistory instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(DocumentHistory instance) {
+		log.debug("attaching clean DocumentHistory instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(DocumentHistory persistentInstance) {
+		log.debug("deleting DocumentHistory instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public DocumentHistory merge(DocumentHistory detachedInstance) {
+		log.debug("merging DocumentHistory instance");
+		try {
+			DocumentHistory result = (DocumentHistory) sessionFactory
+					.getCurrentSession().merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public DocumentHistory findById(long id) {
+		log.debug("getting DocumentHistory instance with id: " + id);
+		try {
+			DocumentHistory instance = (DocumentHistory) sessionFactory
+					.getCurrentSession().get("ee.adit.dao.DocumentHistory", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(DocumentHistory instance) {
+		log.debug("finding DocumentHistory instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession().createCriteria(
+					"ee.adit.dao.DocumentHistory")
+					.add(Example.create(instance)).list();
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
---
+++ src/main/java/ee/adit/dao/RequestLogHome.java
@@ -0,0 +1,123 @@
+package ee.adit.dao;
+
+// Generated 21.05.2010 16:49:44 by Hibernate Tools 3.2.4.GA
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class RequestLog.
+ * @see ee.adit.dao.RequestLog
+ * @author Hibernate Tools
+ */
+public class RequestLogHome {
+
+	private static final Log log = LogFactory.getLog(RequestLogHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext()
+					.lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException(
+					"Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(RequestLog transientInstance) {
+		log.debug("persisting RequestLog instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(RequestLog instance) {
+		log.debug("attaching dirty RequestLog instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(RequestLog instance) {
+		log.debug("attaching clean RequestLog instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(RequestLog persistentInstance) {
+		log.debug("deleting RequestLog instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public RequestLog merge(RequestLog detachedInstance) {
+		log.debug("merging RequestLog instance");
+		try {
+			RequestLog result = (RequestLog) sessionFactory.getCurrentSession()
+					.merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public RequestLog findById(long id) {
+		log.debug("getting RequestLog instance with id: " + id);
+		try {
+			RequestLog instance = (RequestLog) sessionFactory
+					.getCurrentSession().get("ee.adit.dao.RequestLog", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(RequestLog instance) {
+		log.debug("finding RequestLog instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession().createCriteria(
+					"ee.adit.dao.RequestLog").add(Example.create(instance))
+					.list();
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
