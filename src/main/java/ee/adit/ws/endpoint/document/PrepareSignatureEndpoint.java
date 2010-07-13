170856963d4054148549843c6d903603c8c5ecb7
jaaklember <jaaklember@ria.ee>
2010-07-13 15:45:05 +0000

    Lisatud digiallkirjastamise skelett

--- src/main/java/ee/adit/ws/endpoint/document/PrepareSignatureEndpoint.java
+++ src/main/java/ee/adit/ws/endpoint/document/PrepareSignatureEndpoint.java
@@ -1,26 +1,246 @@
 package ee.adit.ws.endpoint.document;
 
+import java.io.File;
+import java.security.cert.X509Certificate;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.Locale;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
+import ee.adit.dao.pojo.AditUser;
+import ee.adit.dao.pojo.Document;
+import ee.adit.dao.pojo.DocumentSharing;
+import ee.adit.exception.AditException;
+import ee.adit.pojo.ArrayOfMessage;
+import ee.adit.pojo.Message;
+import ee.adit.pojo.PrepareSignatureRequest;
+import ee.adit.pojo.PrepareSignatureResponse;
+import ee.adit.service.DocumentService;
+import ee.adit.service.UserService;
+import ee.adit.util.CustomXTeeHeader;
+import ee.adit.util.Util;
 import ee.adit.ws.endpoint.AbstractAditBaseEndpoint;
+import ee.sk.digidoc.DataFile;
+import ee.sk.digidoc.Signature;
+import ee.sk.digidoc.SignedDoc;
+import ee.sk.digidoc.factory.SignatureFactory;
+import ee.sk.utils.ConfigManager;
 import ee.webmedia.xtee.annotation.XTeeService;
 
 @XTeeService(name = "prepareSignature", version = "v1")
 @Component
 public class PrepareSignatureEndpoint extends AbstractAditBaseEndpoint {
+	private static Logger LOG = Logger.getLogger(PrepareSignatureEndpoint.class);
+	private UserService userService;
+	private DocumentService documentService;
 
+	public UserService getUserService() {
+		return userService;
+	}
+
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+
+	public DocumentService getDocumentService() {
+		return documentService;
+	}
+
+	public void setDocumentService(DocumentService documentService) {
+		this.documentService = documentService;
+	}
+	
 	@Override
 	protected Object invokeInternal(Object requestObject) throws Exception {
+		PrepareSignatureResponse response = new PrepareSignatureResponse();
+		ArrayOfMessage messages = new ArrayOfMessage();
 		Date requestDate = Calendar.getInstance().getTime();
 		String additionalInformationForLog = null;
 		Long documentId = null;
 		
-		// TODO Auto-generated method stub
+		try {
+			LOG.debug("prepareSignature.v1 invoked.");
+			PrepareSignatureRequest request = (PrepareSignatureRequest) requestObject;
+			if (request != null) {
+				documentId = request.getDocumentId();
+			}
+			CustomXTeeHeader header = this.getHeader();
+			String applicationName = header.getInfosysteem();
+
+			// Log request
+			Util.printHeader(header);
+			printRequest(request);
+
+			// Check header for required fields
+			checkHeader(header);
+
+			// Check request body
+			checkRequest(request);
+
+			// Kontrollime, kas päringu käivitanud infosüsteem on ADITis
+			// registreeritud
+			boolean applicationRegistered = this.getUserService().isApplicationRegistered(applicationName);
+			if (!applicationRegistered) {
+				String errorMessage = this.getMessageSource().getMessage("application.notRegistered", new Object[] { applicationName }, Locale.ENGLISH);
+				throw new AditException(errorMessage);
+			}
+
+			// Kontrollime, kas päringu käivitanud infosüsteem tohib
+			// andmeid muuta
+			int accessLevel = this.getUserService().getAccessLevel(applicationName);
+			if (accessLevel != 2) {
+				String errorMessage = this.getMessageSource().getMessage("application.insufficientPrivileges.write", new Object[] { applicationName }, Locale.ENGLISH);
+				throw new AditException(errorMessage);
+			}
+
+			// Kontrollime, kas päringus märgitud isik on teenuse kasutaja
+			String userCode = ((this.getHeader().getAllasutus() != null) && (this.getHeader().getAllasutus().length() > 0)) ? this.getHeader().getAllasutus() : this.getHeader().getIsikukood();
+			AditUser user = this.getUserService().getUserByID(userCode);
+			if (user == null) {
+				String errorMessage = this.getMessageSource().getMessage("user.nonExistent", new Object[] { userCode },	Locale.ENGLISH);
+				throw new AditException(errorMessage);
+			}
+
+			// Kontrollime, et kasutajakonto ligipääs poleks peatatud (kasutaja lahkunud)
+			if ((user.getActive() == null) || !user.getActive()) {
+				String errorMessage = this.getMessageSource().getMessage("user.inactive", new Object[] { userCode }, Locale.ENGLISH);
+				throw new AditException(errorMessage);
+			}
+
+			// Now it is safe to load the document from database
+			// (and even necessary to do all the document-specific checks)
+			Document doc = this.documentService.getDocumentDAO().getDocument(request.getDocumentId());
+
+			// Check whether the document exists
+			if (doc == null) {
+				LOG.debug("Requested document does not exist. Document ID: " + request.getDocumentId());
+				String errorMessage = this.getMessageSource().getMessage("document.nonExistent", new Object[] { request.getDocumentId() },	Locale.ENGLISH);
+				throw new AditException(errorMessage);
+			}
+			
+			// Check whether the document is marked as deleted
+			if ((doc.getDeleted() != null) && doc.getDeleted()) {
+				LOG.debug("Requested document is deleted. Document ID: " + request.getDocumentId());
+				String errorMessage = this.getMessageSource().getMessage("document.nonExistent", new Object[] { request.getDocumentId() }, Locale.ENGLISH);
+				throw new AditException(errorMessage);
+			}
+			
+			// Check whether the document is marked as deflated
+			if ((doc.getDeflated() != null) && doc.getDeflated()) {
+				LOG.debug("Requested document is deflated. Document ID: " + request.getDocumentId());
+				String errorMessage = this.getMessageSource().getMessage("document.deflated", new Object[] { doc.getDeflateDate() }, Locale.ENGLISH);
+				throw new AditException(errorMessage);
+			}
+			
+			// Check whether the document is marked as signable
+			if ((doc.getSignable() != null) && doc.getSignable()) {
+				LOG.debug("Requested document is not signable. Document ID: " + request.getDocumentId());
+				String errorMessage = this.getMessageSource().getMessage("document.notSignable", new Object[] { doc.getDeflateDate() }, Locale.ENGLISH);
+				throw new AditException(errorMessage);
+			}
+
+
+			// Document can be signed only if:
+			// a) document belongs to user
+			// b) document is sent or shared to user
+			boolean isOwner = false;
+			if (doc.getCreatorCode().equalsIgnoreCase(userCode)) {
+				isOwner = true;
+			} else {
+				if ((doc.getDocumentSharings() != null) && (!doc.getDocumentSharings().isEmpty())) {
+					Iterator it = doc.getDocumentSharings().iterator();
+					while (it.hasNext()) {
+						DocumentSharing sharing = (DocumentSharing)it.next();
+						if (sharing.getUserCode().equalsIgnoreCase(userCode)) {
+							isOwner = true;
+							break;
+						}
+					}
+				}
+			}
+			
+			if (isOwner) {
+				// TODO:
+				ConfigManager.init("jar://JDigiDoc.cfg");
+				SignedDoc sdoc = new SignedDoc(SignedDoc.FORMAT_DIGIDOC_XML, SignedDoc.VERSION_1_3);
+				DataFile df = sdoc.addDataFile(new File(""), "", DataFile.CONTENT_EMBEDDED_BASE64);
+				X509Certificate cert = sdoc.readCertificate("Cert file");
+				Signature sig = sdoc.prepareSignature(cert,
+						null, // String[] claimedRoles,
+						null); // SignatureProductionPlace address
+				byte[] sidigest = sig.calculateSignedInfoDigest();
+				sdoc.writeToFile(new File("<faili-asukoht-ja-nimi>"));
+				
+				this.documentService.getDocumentDAO().save(doc, null);
+			} else {
+				LOG.debug("Requested document does not belong to user. Document ID: " + request.getDocumentId() + ", User ID: " + userCode);
+				String errorMessage = this.getMessageSource().getMessage("document.doesNotBelongToUser", new Object[] { request.getDocumentId(), userCode }, Locale.ENGLISH);
+				throw new AditException(errorMessage);
+			}
+			
+			// TODO: Teavituskalendri kaudu teavituse saatmine
+				
+
+			// Set response messages
+			response.setSuccess(true);
+			messages.addMessage(new Message("en", this.getMessageSource().getMessage("request.modifyStatus.success", new Object[] { }, Locale.ENGLISH)));
+			response.setMessages(messages);
+		} catch (Exception e) {
+			LOG.error("Exception: ", e);
+			additionalInformationForLog = "Request failed: " + e.getMessage();
+			response.setSuccess(false);
+			ArrayOfMessage arrayOfMessage = new ArrayOfMessage();
+
+			if (e instanceof AditException) {
+				LOG.debug("Adding exception message to response object.");
+				arrayOfMessage.getMessage().add(new Message("en", e.getMessage()));
+			} else {
+				arrayOfMessage.getMessage().add(new Message("en", "Service error"));
+			}
+
+			LOG.debug("Adding exception messages to response object.");
+			response.setMessages(arrayOfMessage);
+		}
+		
 		super.logCurrentRequest(documentId, requestDate, additionalInformationForLog);
 		return null;
 	}
 
+	private void checkHeader(CustomXTeeHeader header) throws Exception {
+		String errorMessage = null;
+		if(header != null) {
+			if ((header.getIsikukood() == null) || (header.getIsikukood().length() < 1)) {
+				errorMessage = this.getMessageSource().getMessage("request.header.undefined.personalCode", new Object[] {}, Locale.ENGLISH);
+				throw new AditException(errorMessage);
+			} else if ((header.getInfosysteem() == null) || (header.getInfosysteem().length() < 1)) {
+				errorMessage = this.getMessageSource().getMessage("request.header.undefined.systemName", new Object[] {}, Locale.ENGLISH);
+				throw new AditException(errorMessage);
+			} else if ((header.getAsutus() == null) || (header.getAsutus().length() < 1)) {
+				errorMessage = this.getMessageSource().getMessage("request.header.undefined.institution", new Object[] {}, Locale.ENGLISH);
+				throw new AditException(errorMessage);
+			}
+		}
+	}
+	
+	private void checkRequest(PrepareSignatureRequest request) {
+		String errorMessage = null; 
+		if(request != null) {
+			if(request.getDocumentId() <= 0) {
+				errorMessage = this.getMessageSource().getMessage("request.body.undefined.documentId", new Object[] {}, Locale.ENGLISH);
+				throw new AditException(errorMessage);
+			}
+		} else {
+			errorMessage = this.getMessageSource().getMessage("request.body.empty", new Object[] {}, Locale.ENGLISH);
+			throw new AditException(errorMessage);
+		}
+	}
+	
+	private static void printRequest(PrepareSignatureRequest request) {
+		LOG.debug("-------- PrepareSignatureRequest -------");
+		LOG.debug("Document ID: " + String.valueOf(request.getDocumentId()));
+		LOG.debug("----------------------------------------");
+	}
 }
