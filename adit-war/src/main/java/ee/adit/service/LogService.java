efb14fea8981b3e13688752785eebd212a0f5e41
jaaklember <jaaklember@ria.ee>
2011-05-03 09:47:47 +0000

    Refaktooritud koodi ja täiendatud kommentaare, et checkstyle tööriist ei annaks ühtegi hoiatust.

--- adit-war/src/main/java/ee/adit/service/LogService.java
+++ adit-war/src/main/java/ee/adit/service/LogService.java
@@ -15,50 +15,64 @@ import ee.adit.dao.pojo.RequestLog;
 
 /**
  * Logging service. Provides methods for inserting log records to database.
- * 
+ *
  * @author Marko Kurm, Microlink Eesti AS, marko.kurm@microlink.ee
  * @author Jaak Lember, Interinx, jaak@interinx.com
  */
 public class LogService {
-
+    /**
+     * Default logger.
+     */
     private static Logger logger = Logger.getLogger(LogService.class);
 
     /**
-     * Error log level WARN
+     * Error log level WARN.
      */
     public static final String ERROR_LOG_LEVEL_WARN = "WARN";
 
     /**
-     * Error log level ERROR
+     * Error log level ERROR.
      */
     public static final String ERROR_LOG_LEVEL_ERROR = "ERROR";
 
     /**
-     * Error log level FATAL
+     * Error log level FATAL.
      */
     public static final String ERROR_LOG_LEVEL_FATAL = "FATAL";
 
     /**
-     * Request log success message
+     * Request log success message.
      */
     public static final String REQUEST_LOG_SUCCESS = "Success";
 
     /**
-     * Request log success message
+     * Request log success message.
      */
     public static final String REQUEST_LOG_FAIL = "ERROR: ";
 
+    /**
+     * Data access object for reading and writing request log data.
+     */
     private RequestLogDAO requestLogDAO;
 
+    /**
+     * Data access object for reading and writing error log data.
+     */
     private ErrorLogDAO errorLogDAO;
 
+    /**
+     * Data access object for reading and writing download request log data.
+     */
     private DownloadRequestLogDAO downloadRequestLogDAO;
 
+    /**
+     * Data access object for reading and writing metadata request log data.
+     */
     private MetadataRequestLogDAO metadataRequestLogDAO;
 
     /**
      * Adds a record to request log.
-     * 
+     *
      * @param requestName
      *            request name
      * @param documentId
@@ -75,8 +89,10 @@ public class LogService {
      *            additional information
      * @return ID of added log entry
      */
-    public Long addRequestLogEntry(String requestName, Long documentId, Date requestDate,
-            String remoteApplicationShortName, String userCode, String organizationCode, String additionalInformation) {
+    public final Long addRequestLogEntry(
+        final String requestName, final Long documentId, final Date requestDate,
+        final String remoteApplicationShortName, final String userCode,
+        final String organizationCode, final String additionalInformation) {
 
         RequestLog logEntry = new RequestLog();
         logEntry.setAdditionalInformation(additionalInformation);
@@ -92,7 +108,7 @@ public class LogService {
 
     /**
      * Adds a record to the error log.
-     * 
+     *
      * @param actionName
      *            the name of the action that caused the error
      * @param documentId
@@ -107,11 +123,13 @@ public class LogService {
      *            error level
      * @param errorMessage
      *            error message
-     * 
+     *
      * @return log entry ID
      */
-    public Long addErrorLogEntry(String actionName, Long documentId, Date errorDate, String remoteApplicationShortName,
-            String userCode, String errorLevel, String errorMessage) {
+    public final Long addErrorLogEntry(
+        final String actionName, final Long documentId, final Date errorDate,
+        final String remoteApplicationShortName, final String userCode,
+        final String errorLevel, final String errorMessage) {
 
         ErrorLog logEntry = new ErrorLog();
         logEntry.setActionName(actionName);
@@ -127,7 +145,7 @@ public class LogService {
 
     /**
      * Adds a record to the download log.
-     * 
+     *
      * @param documentId
      *            document ID associated with the request
      * @param documentFileId
@@ -142,8 +160,10 @@ public class LogService {
      *            the code of the organization that sent the request
      * @return log entry ID
      */
-    public Long addDownloadRequestLogEntry(Long documentId, Long documentFileId, Date requestDate, String appName,
-            String userCode, String orgCode) {
+    public final Long addDownloadRequestLogEntry(
+        final Long documentId, final Long documentFileId,
+        final Date requestDate, final String appName, final String userCode,
+        final String orgCode) {
 
         Long result = 0L;
         try {
@@ -164,7 +184,7 @@ public class LogService {
 
     /**
      * Adds a record to the metadata log.
-     * 
+     *
      * @param documentId
      *            document ID associated with the request
      * @param requestDate
@@ -177,8 +197,9 @@ public class LogService {
      *            the code of the organization that sent the request
      * @return log entry ID
      */
-    public Long addMetadataRequestLogEntry(Long documentId, Date requestDate, String appName, String userCode,
-            String orgCode) {
+    public final Long addMetadataRequestLogEntry(
+        final Long documentId, final Date requestDate, final String appName,
+        final String userCode, final String orgCode) {
 
         Long result = 0L;
         try {
@@ -197,58 +218,105 @@ public class LogService {
     }
 
     /**
-     * Retrieves the request log DAO
-     * 
+     * Retrieves the request log DAO.
+     *
      * @return {@link RequestLogDAO} object that is used for saving
      *         {@link RequestLog} entries to database
      */
-    public RequestLogDAO getRequestLogDAO() {
+    public final RequestLogDAO getRequestLogDAO() {
         return requestLogDAO;
     }
 
     /**
-     * Sets the request log DAO
-     * 
-     * @param requestLogDAO
-     *            {@link RequestLogDAO} object that will be used for saving
-     *            {@link RequestLog} entries to database
+     * Sets the request log DAO.
+     *
+     * @param value
+     *     {@link RequestLogDAO} object that will be used for saving
+     *     {@link RequestLog} entries to database
      */
-    public void setRequestLogDAO(RequestLogDAO requestLogDAO) {
-        this.requestLogDAO = requestLogDAO;
+    public final void setRequestLogDAO(final RequestLogDAO value) {
+        this.requestLogDAO = value;
     }
 
     /**
-     * Adds a request log entry,
-     * 
+     * Adds a request log entry.
+     *
      * @param requestLogEntry
-     *            Log entry as {@link RequestLog} object.
+     *     Log entry as {@link RequestLog} object.
      * @return ID of added log entry
      */
-    public Long addRequestLogEntry(RequestLog requestLogEntry) {
+    public final Long addRequestLogEntry(final RequestLog requestLogEntry) {
         return this.requestLogDAO.save(requestLogEntry);
     }
 
-    public ErrorLogDAO getErrorLogDAO() {
+    /**
+     * Gets data access object for reading and writing error log data.
+     *
+     * @return
+     *     Data access object for reading and writing error log data.
+     */
+    public final ErrorLogDAO getErrorLogDAO() {
         return errorLogDAO;
     }
 
-    public void setErrorLogDAO(ErrorLogDAO errorLogDAO) {
-        this.errorLogDAO = errorLogDAO;
+    /**
+     * Sets data access object for reading and writing error log data.
+     *
+     * @param value
+     *     {@link ErrorLogDAO} object that will be used for saving
+     *     {@link ErrorLog} entries to database
+     */
+    public final void setErrorLogDAO(final ErrorLogDAO value) {
+        this.errorLogDAO = value;
     }
 
-    public DownloadRequestLogDAO getDownloadRequestLogDAO() {
+    /**
+     * Gets data access object for reading and writing download
+     * request log data.
+     *
+     * @return
+     *     Data access object for reading and writing download
+     *     request log data.
+     */
+    public final DownloadRequestLogDAO getDownloadRequestLogDAO() {
         return downloadRequestLogDAO;
     }
 
-    public void setDownloadRequestLogDAO(DownloadRequestLogDAO downloadRequestLogDAO) {
-        this.downloadRequestLogDAO = downloadRequestLogDAO;
+    /**
+     * Sets data access object for reading and writing download
+     * request log data.
+     *
+     * @param value
+     *     {@link DownloadRequestLogDAO} object that will be used for saving
+     *     {@link DownloadRequestLog} entries to database
+     */
+    public final void setDownloadRequestLogDAO(
+        final DownloadRequestLogDAO value) {
+        this.downloadRequestLogDAO = value;
     }
 
-    public MetadataRequestLogDAO getMetadataRequestLogDAO() {
+    /**
+     * Gets data access object for reading and writing metadata
+     * request log data.
+     *
+     * @return
+     *     Data access object for reading and writing metadata
+     *     request log data.
+     */
+    public final MetadataRequestLogDAO getMetadataRequestLogDAO() {
         return metadataRequestLogDAO;
     }
 
-    public void setMetadataRequestLogDAO(MetadataRequestLogDAO metadataRequestLogDAO) {
-        this.metadataRequestLogDAO = metadataRequestLogDAO;
+    /**
+     * Sets data access object for reading and writing metadata
+     * request log data.
+     *
+     * @param value
+     *     {@link MetadataRequestLogDAO} object that will be used for saving
+     *     {@link MetadataRequestLog} entries to database
+     */
+    public final void setMetadataRequestLogDAO(
+        final MetadataRequestLogDAO value) {
+        this.metadataRequestLogDAO = value;
     }
 }
