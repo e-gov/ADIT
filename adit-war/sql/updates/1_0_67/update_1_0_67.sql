4c447bd2b504efdd0e954de589a3c78b4cc18008
Aleksei Kokarev <>
2014-06-16 15:47:06 +0300

    added distinct to dhl_id query

--- adit-war/sql/updates/1_0_67/update_1_0_67.sql
+++ adit-war/sql/updates/1_0_67/update_1_0_67.sql
@@ -1,7 +1,7 @@
 --!!!!!! this script needs select priveleges on adit_dvk schemas DHL_MESSAGE_RECIPIENT table and select/update priveleges on adit schemas document, document_sharing tables!!!!!
 update &&ADIT_SCHEMA..document_sharing docsh set docsh.dvk_id=
   (
-    select mr.dhl_id from &&ADIT_DVK_SCHEMA..DHL_MESSAGE_RECIPIENT mr join &&ADIT_DVK_SCHEMA..dhl_message dm on dm.dhl_message_id=mr.dhl_message_id where mr.dhl_message_id=
+    select distinct(mr.dhl_id) from &&ADIT_DVK_SCHEMA..DHL_MESSAGE_RECIPIENT mr join &&ADIT_DVK_SCHEMA..dhl_message dm on dm.dhl_message_id=mr.dhl_message_id where mr.dhl_message_id=
       (select doc.dvk_id from &&ADIT_SCHEMA..document doc join &&ADIT_SCHEMA..document_sharing ds on ds.document_id=doc.id where ds.id=docsh.id) and dm.is_incoming=0
   ) 
   where docsh.id in 
