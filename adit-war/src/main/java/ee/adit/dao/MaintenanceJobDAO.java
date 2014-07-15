d2652cb7e773a4d55177d976cdd6c77b9ace4625
Hendrik Pärna <hendrik.parna@icefire.ee>
2014-07-15 16:24:55 +0300

    ADIT-13

--- adit-war/src/main/java/ee/adit/dao/MaintenanceJobDAO.java
+++ adit-war/src/main/java/ee/adit/dao/MaintenanceJobDAO.java
@@ -1,8 +1,12 @@
 package ee.adit.dao;
 
+import ee.adit.dao.pojo.MaintenanceJob;
+import ee.adit.exception.AditInternalException;
 import java.sql.SQLException;
 
+import java.util.List;
 import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -56,5 +60,55 @@ public class MaintenanceJobDAO extends HibernateDaoSupport {
 
         logger.debug("setJobRunningStatus result for job " + jobId + " is: " + result.getResultCode());
         return ("ok".equalsIgnoreCase(result.getResultCode()));
+    }
+
+    /**
+     * Initialize running job statuses.
+     */
+    public void initializeJobRunningStatuses() {
+        logger.debug("initializeJobRunningStatuses starting...");
+        setAllJobsToNotRunning(findAllJobs());
+    }
+
+    /**
+     * Find all jobs.
+     * @return list of maintenance jobs.
+     */
+    public List<MaintenanceJob> findAllJobs() {
+        Session session = null;
+        try {
+            session = this.getSessionFactory().openSession();
+            Criteria criteria = session.createCriteria(MaintenanceJob.class);
+            return (List<MaintenanceJob>) criteria.list();
+        } catch (Exception e) {
+            throw new AditInternalException("Error while fetching DVK DocumentSharings: ", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    private void setAllJobsToNotRunning(final List<MaintenanceJob> jobs) {
+        if (jobs != null) {
+            logger.debug("found jobs: " + jobs.size());
+
+            for (MaintenanceJob maintenanceJob: jobs) {
+                Session session = null;
+                try {
+                    //clear the state
+                    if (maintenanceJob.isRunning()) {
+                        setJobRunningStatus(maintenanceJob.getId(), false);
+                        logger.debug("job: " + maintenanceJob.getName() + " running status now changed to false");
+                    }
+                } catch (Exception e) {
+                    throw new AditInternalException("Error while fetching DVK DocumentSharings: ", e);
+                } finally {
+                    if (session != null) {
+                        session.close();
+                    }
+                }
+            }
+        }
     }
 }
